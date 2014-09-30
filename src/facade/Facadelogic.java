7d6bfbc5bb90b1a44330475a2a4761c020b18947
bobkoobg <boyko.surlev@gmail.com>
2014-09-30 19:29:05 +0200

    Fully working Facade with Database Minor fixes in Person Changed names of FacadeInterface (Teacher's example sucked .. wrong names etc.) (BetaProject class becomes pointless at that point) I still have psvm in Facade. v0.4

--- src/facade/Facadelogic.java
+++ src/facade/Facadelogic.java
@@ -1,14 +1,21 @@
 package facade;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import entity.Person;
 import entity.RoleSchool;
+import entity.Teacher;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class Facadelogic implements FacadeInterface {
 
@@ -21,12 +28,12 @@ public class Facadelogic implements FacadeInterface {
     EntityTransaction tr;
     //Vital for the parsing to Gson
     private Gson gson = new Gson();
-    //*************************************************
-    Map<Integer, Person> people = new HashMap();
 
     @Override
     public String getPersonsAsJSON() {
-        return ((people.isEmpty()) ? null : people.values().toString());
+        Query query = em.createQuery("SELECT a FROM Person a");
+        List<Person> nesquick = (List<Person>) query.getResultList();
+        return gson.toJson(nesquick);
     }
 
     @Override
@@ -48,12 +55,40 @@ public class Facadelogic implements FacadeInterface {
 
     @Override
     public RoleSchool addRoleFromJSON(String json, Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Person a = em.find(Person.class, id);
+        JsonElement jelement = new JsonParser().parse(json);
+        JsonObject jobject = jelement.getAsJsonObject();
+        JsonElement currentrole = jobject.get("roleName");
+        String roleToString = currentrole.getAsString();
+        System.out.println("yoloswag " + roleToString);
+        tr.begin();
+        Teacher t;
+        switch (roleToString) {
+            case "Teacher":
+                JsonElement currentdegree = jobject.get("degree");
+                String degreeToString = currentdegree.getAsString();
+                t = new Teacher(degreeToString);
+                a.addRole(t);
+                em.persist(t);
+                break;
+            default:
+                return null;
+        }
+
+        tr.commit();
+        return t;
+
     }
 
     @Override
     public Person deletePersonFromJSON(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        tr.begin();
+        Person a = em.find(Person.class, id);
+        if (a != null) {
+            em.remove(a);
+        }
+        tr.commit();
+        return a;
     }
 
     private void initializeTransactions() {
@@ -70,7 +105,12 @@ public class Facadelogic implements FacadeInterface {
         System.out.println("Object: " + p.toString());
         System.out.println("Object: " + p2.toString());
         System.out.println("Object: " + p3.toString());
+        System.out.println("loraloralorao>O " + getPersonsAsJSON());
         System.out.println("GetParticularPerson :" + getPersonAsJSON(100));
+        String t1 = "{\"degree\":\"d-1\", \"roleName\":\"Teacher\" }";
+        addRoleFromJSON(t1, 100);
+        deletePersonFromJSON(102);
+
     }
 
     public static void main(String[] args) {
