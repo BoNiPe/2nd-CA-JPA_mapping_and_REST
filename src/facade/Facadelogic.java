8251e3e74b698c314cc73dadb005be88cf88817c
Desting <Nik_zd@hotmail.com>
2014-10-01 13:13:24 +0200

    Project to part 3? With HTTP Server - needs to be tested properly

---
+++ src/facade/Facadelogic.java
@@ -0,0 +1,145 @@
+package facade;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import entity.Person;
+import entity.RoleSchool;
+import entity.Teacher;
+import exceptions.NotFoundException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Facadelogic implements FacadeInterface {
+
+    private static Facadelogic instance = new Facadelogic();
+    
+    public static Facadelogic getFacade(boolean reseet){
+    if(true){
+      instance = new Facadelogic();
+    }
+    return instance;
+  }
+    
+    
+    //Interface used to interact with the entity manager factory for the persistence unit.
+    //Persistence knows where to put the objects and afterwards the Entity Manager
+    //takes the whole information, from which it "createsEntity"
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("BetaProjectbPU");
+    EntityManager em = emf.createEntityManager();
+    //So that we can have a single transaction after the initialization
+    EntityTransaction tr;
+    //Vital for the parsing to Gson
+    private Gson gson = new Gson();
+
+    @Override
+    public String getPersonsAsJSON() {
+        Query query = em.createQuery("SELECT a FROM Person a");
+        List<Person> nesquick = (List<Person>) query.getResultList();
+        return gson.toJson(nesquick);
+    }
+
+    @Override
+    public String getPersonAsJSON(Integer id) throws NotFoundException {
+        // NIKOLAJ ADDED NOTFOUNDEXCEPTION TO THIS METHOD
+        Person a = em.find(Person.class, id);
+        if(a == null){
+            throw new NotFoundException("No person exists for the given ID.");
+        }
+        
+        return gson.toJson(a);
+    }
+
+    @Override
+    public Person addPersonFromJSON(String json) {
+        initializeTransactions();
+        tr.begin();
+        Person p = gson.fromJson(json, Person.class);
+        em.persist(p);
+        tr.commit();
+        System.out.println("id:" + p.getId() + ", object:  " + p.toString());
+        return p;
+    }
+
+    @Override
+    public RoleSchool addRoleFromJSON(String json, Integer id) {
+        Person a = em.find(Person.class, id);
+        JsonElement jelement = new JsonParser().parse(json);
+        JsonObject jobject = jelement.getAsJsonObject();
+        JsonElement currentrole = jobject.get("roleName");
+        String roleToString = currentrole.getAsString();
+        System.out.println("yoloswag " + roleToString);
+        tr.begin();
+        Teacher t;
+        switch (roleToString) {
+            case "Teacher":
+                JsonElement currentdegree = jobject.get("degree");
+                String degreeToString = currentdegree.getAsString();
+                t = new Teacher(degreeToString);
+                a.addRole(t);
+                em.persist(t);
+                break;
+            default:
+                return null;
+        }
+
+        tr.commit();
+        return t;
+
+    }
+
+    @Override
+    public Person deletePersonFromJSON(Integer id) throws NotFoundException{
+        // NIKOLAJ ADDED NOTFOUNDEXCEPTION TO THIS METHOD
+        tr.begin();
+        Person a = em.find(Person.class, id);
+        if (a == null) {
+            throw new NotFoundException("No person exists for the given id");
+        }
+            em.remove(a);
+        tr.commit();
+        return a;
+    }
+
+    private void initializeTransactions() {
+        tr = em.getTransaction();
+    }
+
+    public void testingCode() throws NotFoundException{
+        String addingperson = "{ \"firstName\":\"John\", \"lastName\":\"McLaren\", \"mail\":\"j@m.uk\", \"phone\":\"3456\" }";
+        String addingperson2 = "{ \"firstName\":\"aaaaaa\", \"lastName\":\"aaaaa\", \"mail\":\"aaaaaa@m.uk\", \"phone\":\"33242346\" }";
+        String addingperson3 = "{ \"firstName\":\"bbbbb\", \"lastName\":\"bbbb\", \"mail\":\"bbbbbb@m.uk\", \"phone\":\"3234324236\" }";
+        Person p = addPersonFromJSON(addingperson);
+        Person p2 = addPersonFromJSON(addingperson2);
+        Person p3 = addPersonFromJSON(addingperson3);
+        System.out.println("Object: " + p.toString());
+        System.out.println("Object: " + p2.toString());
+        System.out.println("Object: " + p3.toString());
+        System.out.println("loraloralorao>O " + getPersonsAsJSON());
+        
+            System.out.println("GetParticularPerson :" + getPersonAsJSON(100));
+        
+        String t1 = "{\"degree\":\"d-1\", \"roleName\":\"Teacher\" }";
+        addRoleFromJSON(t1, 100);
+
+            deletePersonFromJSON(102);
+
+
+    }
+
+    public static void main(String[] args) throws NotFoundException {
+
+        new Facadelogic().testingCode();
+    }
+
+}
---
+++ test/TestPerson.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import entity.Person;
+import entity.Student;
+import entity.Teacher;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Desting
+ */
+public class TestPerson {
+    Person per;
+    
+    public TestPerson() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        per = new Person("Lars", "IsCool", "12345" , "BIF@Forever.dk");
+        
+    }
+    
+    @After
+    public void tearDown() {
+        
+    }
+    
+    @Test
+    public void changeInfo(){
+        per.setFirstName("a");
+        per.setLastName("b");
+        per.setPhone("1");
+        per.setEmail("c");
+        
+        
+        assertEquals("Name: a b with email: c and phone number: 1", per.toString());
+    }
+    
+    @Test
+    public void testWtestDB(){
+        Person per2 = new Person("Nikolaj", "Desting", "555", "haha@lol.dk");
+        ArrayList list = new ArrayList();
+        
+        list.add(per);
+        list.add(per2);
+        
+        assertEquals(2, list.size());
+        assertEquals("[Name: Lars IsCool with email: BIF@Forever.dk and phone number: 12345, Name: Nikolaj Desting with email: haha@lol.dk and phone number: 555]", list.toString());
+    }
+    
+//    @Test
+//    public void addRole(){
+//        Person per2 = new Person("Nikolaj", "Desting", "555", "haha@lol.dk");
+//        Teacher teacher = new Teacher("Computer Science");
+//        Student student = new Student("3rd");
+//        per.addRole(teacher);
+//        per2.addRole(teacher);
+//        per2.addRole(student);
+//        assertEquals("teacher", per.checkRoles());
+//        assertEquals(2, per2.checkRoles());
+//    }
+    
+}
